06c8456b46a937879fdfe985b499541ada469a23
Jayson Barlan <barlanjayson968@gmail.com>
2024-10-04 14:16:57 +0800

    Add files via upload

---
+++ LoopBarlan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LoopBarlan
+{
+	public static void main (String[] args)
+	{
+		for (int i= 1; i <= 5000; ++i)
+		{
+			System.out.println(" Jayson Barlan");
+		}
+	}
+}	
